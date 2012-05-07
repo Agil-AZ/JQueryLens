53d1d07ff89446abd44717ab27b26fc69e2cc2d3
Pepe Doval <pepellou@gmail.com>
2012-05-07 15:03:36 +0200

    Add use sample

---
+++ sample/js/JQueryLens.js
@@ -0,0 +1,157 @@
+JQueryLens = { 
+
+	defaultParams: function() {
+		return {
+			locator: "locator",
+			thumbnail: "thumbnail",
+			lens: "lens",
+			zoom: 4
+		};
+	},
+
+	init: function(params) {
+		params = jQuery.extend(this.defaultParams(), params);
+
+		this._zoom = params.zoom;
+		this.locator = this.getElement(params.locator);
+		this.thumbnail = this.getElement(params.thumbnail);
+		this.lens = this.getElement(params.lens);
+		this.image = $(this.getIdForImage());
+
+		this.resizeLocator();
+		this.resizeThumbnail();
+		this.resizeImage();
+	},
+
+	getIdForImage: function() {
+		return this.getElement(this.lens.attr("id") + " img");
+	},
+
+	getElement: function(id) {
+		return $("#" + id);
+	},
+
+	getBorder : function(element) {
+		return parseInt(element.css("border-left-width"));
+	},
+
+	moveLocatorAccordingToBorder: function() {
+		this.locator.offset({
+			top: this.locator.offset().top + 
+				 this.getBorder(this.thumbnail),
+			left: this.locator.offset().left + 
+				  this.getBorder(this.thumbnail)
+		});
+	},
+
+	moveImageAccordingToBorder: function() {
+		this.image.offset({
+			top: this.image.offset().top + 
+				 this.getBorder(this.lens),
+			left: this.image.offset().left + 
+				  this.getBorder(this.lens)
+		});
+	},
+
+	resizeImage: function() {
+		this.image.width(this.thumbnail.width() * this._zoom);
+		this.image.height(this.thumbnail.height() * this._zoom);
+	},
+
+	resizeLocator: function() {
+		this.locator.width(this.lens.width() / this._zoom);
+		this.locator.height(this.lens.height() / this._zoom);
+	},
+
+	resizeThumbnail: function() {
+		var imageHeight = this.image.height();
+		var imageWidth = this.image.width();
+		this.thumbnail.height(this.thumbnail.width() * imageHeight / imageWidth);
+	},
+
+	refreshImageInLens: function() {
+		var lensTop = Math.ceil(this.lens.position().top);
+		var lensLeft = Math.ceil(this.lens.position().left);
+		this.image.offset({
+			top: lensTop - 
+				 (this.getBorder(this.thumbnail) * this._zoom) - 
+				 (this.locator.position().top * this._zoom) - 
+				 (this.getBorder(this.locator) * this._zoom),
+			left: lensLeft  - 
+				  (this.getBorder(this.thumbnail) * this._zoom) - 
+				  (this.locator.position().left * this._zoom) - 
+				  (this.getBorder(this.locator) * this._zoom)
+		});
+	},
+
+	getDifference: function(innerDiv, outerDiv) {
+		return {
+			vertical: outerDiv.height() - innerDiv.height() - this.getBorder(innerDiv)*2,
+			horizontal: outerDiv.width() - innerDiv.width() - this.getBorder(innerDiv)*2,
+		};
+	},
+
+	getLimits: function(innerDiv, outerDiv) {
+		var outerTop = outerDiv.position().top;
+		var outerLeft = outerDiv.position().left;
+		var difference = this.getDifference(innerDiv, outerDiv);
+		return {
+			minTop: outerTop,
+			maxTop: outerTop + difference.vertical,
+			minLeft: outerLeft,
+			maxLeft: outerLeft + difference.horizontal
+		};
+	},
+
+	locatorLimits: function() {
+		return this.getLimits(this.locator, this.thumbnail);
+	},
+
+	adjust: function(position, limits) {
+		if (position.top < limits.minTop)
+			position.top = limits.minTop;
+
+		if (position.top > limits.maxTop)
+			position.top = limits.maxTop;
+
+		if (position.left < limits.minLeft)
+			position.left = limits.minLeft;
+
+		if (position.left > limits.maxLeft)
+			position.left = limits.maxLeft;
+
+		return position;
+	},
+
+	adjustLocatorPosition: function() {
+		var position = {
+			top: Math.ceil(this.locator.offset().top),
+			left: Math.ceil(this.locator.offset().left)
+		};
+
+		var adjustedPosition = this.adjust(position, this.locatorLimits());
+
+		this.locator.offset(adjustedPosition);
+	},
+
+	refreshLocatorInThumbnail: function(x, y) {
+		this.locator.offset({
+			top: y - (this.locator.height() + this.getBorder(this.locator) * 2) / 2,
+			left: x - (this.locator.width() + this.getBorder(this.locator) * 2) / 2
+		});
+
+		this.adjustLocatorPosition();
+		this.refreshImageInLens();
+		this.moveLocatorAccordingToBorder();
+		this.moveImageAccordingToBorder();
+	},
+};
+
+$(document).ready(function() {
+	JQueryLens.init({
+		zoom: 4
+	});
+	JQueryLens.thumbnail.mousemove(function(e) {
+		JQueryLens.refreshLocatorInThumbnail(e.pageX, e.pageY);
+	});
+});
